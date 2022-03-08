69f304e2ef22e26f34b8ef6e3d3ef54788c083dc
esaddag <dagmuhammedesad@gmail.com>
2022-03-08 17:01:04 +0300

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: "0.0.0.0",
+  server: {
+    port: process.env.PORT
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
